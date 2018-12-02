8ef49228318a20e5cb39baab0456d02054bc3f8c
webaifei <1334806067@qq.com>
2018-12-02 17:24:47 +0800

    基本布局

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {Tabs, Upload} from "element-react";
+import "./App.css";
+import 'element-theme-default';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <Tabs type="border-card" activeName="1">
+          <Tabs.Pane label="上传表格" name="1">
+          <Upload
+            className="upload-demo"
+            drag
+            action="//jsonplaceholder.typicode.com/posts/"
+            multiple
+            tip={<div className="el-upload__tip">上传CVS EXCEL格式</div>}
           >
-            Learn React
-          </a>
-        </header>
+            <i className="el-icon-upload"></i>
+            <div className="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+          </Upload>
+          </Tabs.Pane>
+          <Tabs.Pane label="PV-UV" name="2">PV-UV</Tabs.Pane>
+          <Tabs.Pane label="漏斗模型" name="3">漏斗模型</Tabs.Pane>
+          <Tabs.Pane label="定时补偿任务" name="4">定时补偿任务</Tabs.Pane>
+        </Tabs>
       </div>
     );
   }
