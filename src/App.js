e793c30e1e892a9854d0a4832480683efc562e1c
zhangaifei <zhangaifei@51.com>
2018-12-04 18:01:44 +0800

    添加数据处理

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import {Tabs} from "element-react";
+import utils from "./utils/";
 import PvUv from "./components/pvUv";
 import Upload from "./components/Upload";
 import "./App.css";
 import 'element-theme-default';
+
+// mock use local data
+const data = require("./data/data.json");
+const handledData = utils.convertData(data);
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <Tabs type="border-card" activeName="1">
-          <Tabs.Pane label="上传表格" name="1">
+          <Tabs.Pane label="PV-UV" name="1">
+            <PvUv handledData={handledData}/>
+          </Tabs.Pane>
+          <Tabs.Pane label="漏斗模型" name="2">漏斗模型</Tabs.Pane>
+          <Tabs.Pane label="更新数据" name="3">
             <Upload/>
           </Tabs.Pane>
-          <Tabs.Pane label="PV-UV" name="2">
-            <PvUv/>
-          </Tabs.Pane>
-          <Tabs.Pane label="漏斗模型" name="3">漏斗模型</Tabs.Pane>
-          <Tabs.Pane label="定时补偿任务" name="4">定时补偿任务</Tabs.Pane>
         </Tabs>
       </div>
     );
--- src/components/pvUv.js
+++ src/components/pvUv.js
@@ -1,30 +1,89 @@
 import React, { Component } from 'react';
 import echarts from "echarts";
+import {Tabs} from "element-react";
 import "./style.css";
 export default class PvUv extends Component{
-
-  componentDidMount() {
-    const containerRef = this.refs.container;
-    const myChart = echarts.init(containerRef);
-    myChart.setOption({
-      title: {
-          text: 'ECharts 入门示例'
-      },
-      tooltip: {},
-      xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-      },
-      yAxis: {},
-      series: [{
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-      }]
-  });
-  }
-  render() {
-    return <div className="echart-container" ref="container">
-
-    </div>;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            // 申请办卡页面数据
+            applyCardData: {
+                title: {
+                    text: '申请办卡PV-UV'
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross',
+                        animation: false,
+                        label: {
+                            backgroundColor: '#505765'
+                        }
+                    }
+                },
+                legend: {
+                    data:['PV','UV']
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: 40,
+                    containLabel: true
+                },
+                toolbox: {
+                    feature: {
+                        dataZoom: {
+                            yAxisIndex: 'none'
+                        },
+                        restore: {},
+                        saveAsImage: {}
+                    }
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: this.props.handledData.date
+                },
+                yAxis: {
+                    type: 'value',
+                    boundaryGap: [0, '100%']
+                },
+                dataZoom: [{
+                    show: true,
+                    realtime: true,
+                    start: 75,
+                    end: 100
+                },
+                {
+                    type: 'inside',
+                    realtime: true,
+                    start: 65,
+                    end: 85
+                }],
+                series: [
+                     this.props.handledData.PV,
+                     this.props.handledData.UV
+                    
+                ]
+            }
+        }
+    }
+    componentDidMount() {
+        const containerRef = this.refs.container;
+        console.log(containerRef);
+        
+        const myChart = echarts.init(containerRef);
+        myChart.setOption(this.state.applyCardData);
+    }
+    render() {
+        return <div>
+            <Tabs type="card" activeName="1">
+                <Tabs.Pane label="申请办卡" name="1">
+                    <div ref="container" className="echart-container"></div>
+                </Tabs.Pane>
+                <Tabs.Pane label="个人中心" name="2">
+                </Tabs.Pane>
+            </Tabs>
+        </div>;
+    }
 }
